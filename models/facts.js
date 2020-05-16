fa55ed3a00ad4808c12aa5b365c4e1cdd10f35b0
clenberger <christian.lenberger@gmail.com>
2020-05-15 19:05:01 -0700

    fixing heroku

--- models/facts.js
+++ models/facts.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+
 const FactSchema = new Schema ({
     text: { type: String },
     createdAt: { type: Date, default: Date.now},
